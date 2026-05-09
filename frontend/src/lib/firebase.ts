847c42c074d04cd1a50c05d36fd1c57aa99081af
Dhairya Shah <shahdhairya763@gmail.com>
2026-05-09 17:08:51 +0530

    Update firebase.ts

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0UmQ8caywKIPAa7b_KNvsMqXhUUAPf7o",
-  authDomain: "subscription-management-f8246.firebaseapp.com",
-  projectId: "subscription-management-f8246",
-  storageBucket: "subscription-management-f8246.firebasestorage.app",
-  messagingSenderId: "27585645267",
-  appId: "1:27585645267:web:058306094dc33ecf4ea226",
-  measurementId: "G-M0S2NDR681"
+  apiKey: "YoUrApIkEyUsEd",
+  authDomain: "subscription-management.firebaseapp.com",
+  projectId: "subscription-management",
+  storageBucket: "subscription-management.firebasestorage.app",
+  messagingSenderId: "1234567",
+  appId: "YOURAPPID",
+  measurementId: "yOuRmEaSuReMeNtId"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
